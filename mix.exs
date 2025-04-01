3a79d716da86a65415ee645c4fa601b8153c8fde
Frerich Raabe <frerich.raabe@gmail.com>
2025-04-01 19:34:57 +0200

    Let 'mix release' create tarballs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule AutoUpdaterTestapp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        auto_updater_testapp: [
+          steps: [:assemble, :tar]
+        ]
+      ]
     ]
   end
 
